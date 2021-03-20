74cd7a6fe1e622d2ddeab767312d80da3cebde7e
brianseo19 <brian.seo919@gmail.com>
2021-03-19 22:07:20 -0700

    Initial Commit

---
+++ SwiftChallenge/SwiftChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChallengeApp.swift
+//  SwiftChallenge
+//
+//  Created by Brian Seo on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
